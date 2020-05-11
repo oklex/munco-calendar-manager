4a8b27f12b1c296723f44b621bf54c262f5aaa34
oklex <alexkim.347@gmail.com>
2020-05-11 16:55:09 -0700

    updated card with input wrapper

--- src/components/organizationCards/orgAddCard.tsx
+++ src/components/organizationCards/orgAddCard.tsx
@@ -6,8 +6,15 @@ import {
 } from "../../models/calendar";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { InputWrapper } from "../InputWrapper";
 
-interface IOrgAddCardState extends IOrganizationRequest {}
+interface IOrgAddCardState extends IOrganizationRequest {
+  short_name_warning: string | null,
+  full_name_warning: string | null,
+  organization_type_warning: string | null,
+  website_warning: string | null,
+  running_since_warning: string | null
+}
 
 class OrgAddCard extends React.Component<{}, IOrgAddCardState> {
   state: IOrgAddCardState = {
@@ -16,6 +23,11 @@ class OrgAddCard extends React.Component<{}, IOrgAddCardState> {
     organization_type: IOrganizationType.studentProject,
     website: "",
     running_since: new Date(),
+    short_name_warning: null,
+    full_name_warning:  null,
+    organization_type_warning: null,
+    website_warning: null,
+    running_since_warning: null
   };
 
   submitRequest = () => {
@@ -52,20 +64,19 @@ class OrgAddCard extends React.Component<{}, IOrgAddCardState> {
   showForm = () => {
     return (
       <div>
-        <input
+        <InputWrapper title="short name" warning={this.state.short_name_warning}><input
           type="text"
           placeholder="short name"
           value={this.state.short_name}
           onChange={this.handleShortNameChange}
-        />
-        <br />
+        /></InputWrapper>
+        <InputWrapper title="full name" warning={this.state.full_name_warning}>
         <input
           type="text"
           placeholder="full name"
           value={this.state.full_name}
           onChange={this.handleFullNameChange}
-        />
-        <br />
+        /></InputWrapper>
         {/* <input type="radio" value={this.state.organization_type}/><br/> */}
         <input
           type="text"
