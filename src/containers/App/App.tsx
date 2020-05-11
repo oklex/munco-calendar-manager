7450b9e8bdfb82b3003f05ea34e8f3df25429e9f
oklex <alexkim.347@gmail.com>
2020-05-11 15:47:25 -0700

    added routes

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -2,16 +2,33 @@ import React from "react";
 import "./App.scss";
 import EditorPanel from "../EditorPanel/EditorPanel";
 import OrganizationList from "../OrganizationLIst/OrganizationList";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-class App extends React.Component<{},{}> {
+class App extends React.Component<{}, {}> {
+  showOrgList = () => {
+    return <OrganizationList />;
+  };
+
+  showEditor = () => {
+    return <EditorPanel/>
+  }
+
+  show404 = () => {
+    return <div>Error 404</div>
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <OrganizationList/>
-          <EditorPanel/>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <meta name="robots" content="noindex" />
+          <Switch>
+            <Route exact path="/" component={this.showOrgList} />
+            <Route exact path="/:website_key" component={this.showEditor} />
+            <Route component={this.show404}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
