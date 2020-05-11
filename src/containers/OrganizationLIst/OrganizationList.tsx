5985b8e9ab552ca9d11de4746c10997934ea5f55
oklex <alexkim.347@gmail.com>
2020-05-11 15:47:44 -0700

    formatted org list

--- src/containers/OrganizationLIst/OrganizationList.tsx
+++ src/containers/OrganizationLIst/OrganizationList.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import { IOrganization } from "../../models/calendar";
+import { CalendarService } from "../../services/OrganizationServices";
+import './OrganizationList.scss'
 
-export default class OrganizationList extends React.Component<{},{}> {
-    /* 
+interface IOrganizationListState {
+  list: IOrganization[];
+}
+
+export default class OrganizationList extends React.Component<
+  {},
+  IOrganizationListState
+> {
+  /* 
     ComponentDidMount : load in all data from calendar API (/api/organizations/:id?include=all)
 
     functions:
@@ -10,8 +20,33 @@ export default class OrganizationList extends React.Component<{},{}> {
     - showSingleAppCard
 
     */
-    
-    render() {
-        return (<div>Prototype OrganizationList</div>)
-    }
+  state = {
+    list: [],
+  };
+
+  componentDidMount = async () => {
+    let list: IOrganization[] = await CalendarService.getAllOrganizations();
+    this.setState({
+      list,
+    });
+  };
+
+  showAllOrganizations = () => {
+      return this.state.list.map((org) => {
+          return this.showSingleOrganization(org)
+      })
+  }
+
+  showSingleOrganization = (org: IOrganization) => {
+      return (<div className='organizationSelect'>
+          <h3>{org.short_name}</h3>
+          <p>{org.full_name}</p>
+      </div>)
+  }
+
+  render() {
+    return <div className='organizationList'>Prototype OrganizationList
+        {this.showAllOrganizations()}
+    </div>;
+  }
 }
