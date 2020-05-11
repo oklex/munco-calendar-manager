e251b2c600e646f2d6453bf2b002d35041b5aa89
oklex <alexkim.347@gmail.com>
2020-05-11 16:37:21 -0700

    added onChange functions

--- src/components/organizationCards/orgAddCard.tsx
+++ src/components/organizationCards/orgAddCard.tsx
@@ -1,41 +1,95 @@
-import React from 'react'
-import { IOrganization, IOrganizationType, IOrganizationRequest } from '../../models/calendar'
+import React from "react";
+import {
+  IOrganization,
+  IOrganizationType,
+  IOrganizationRequest,
+} from "../../models/calendar";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 interface IOrgAddCardState extends IOrganizationRequest {}
 
-class OrgAddCard extends React.Component<{},IOrgAddCardState> {
-    state:IOrgAddCardState = {
-        short_name: "short_name",
-        full_name: "full_name",
-        organization_type: IOrganizationType.studentProject,
-        website: "test.com",
-        running_since: new Date()
-    }
+class OrgAddCard extends React.Component<{}, IOrgAddCardState> {
+  state: IOrgAddCardState = {
+    short_name: "",
+    full_name: "",
+    organization_type: IOrganizationType.studentProject,
+    website: "",
+    running_since: new Date(),
+  };
 
-    submitRequest = () => {
-        // use state to POST /organizations/new
-    }
+  submitRequest = () => {
+    // use state to POST /organizations/new
+  };
 
-    showForm = () => {
-        return (<div>
-            <input type="text" value={this.state.short_name}/><br/>
-            <input type="text" value={this.state.full_name}/><br/>
-            <input type="text" value={this.state.organization_type}/><br/>
-            <input type="text" value={this.state.website}/><br/>
-            <DatePicker
-            onChange={() => {}}
-        selected={this.state.running_since} />
-        </div>)
-    }
+  handleShortNameChange = (e: React.FormEvent<HTMLInputElement>) => {
+    this.setState({
+      short_name: e.currentTarget.value,
+    });
+  };
+  handleFullNameChange = (e: React.FormEvent<HTMLInputElement>) => {
+    this.setState({
+      short_name: e.currentTarget.value,
+    });
+  };
+  handleOrgTypeChange = (e: React.FormEvent<HTMLInputElement>) => {
+    // check to see if it's a valid type
+    this.setState({
+      organization_type: e.currentTarget.value,
+    });
+  };
+  handleWebsiteChange = (e: React.FormEvent<HTMLInputElement>) => {
+    this.setState({
+      website: e.currentTarget.value,
+    });
+  };
+  handleDateChange = (date: Date) => {
+    this.setState({
+      running_since: date,
+    });
+  };
 
-    render() {
-        return (<div>
-            Add an organization
-            {this.showForm()}
-        </div>)
-    }
+  showForm = () => {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="short name"
+          value={this.state.short_name}
+          onChange={this.handleShortNameChange}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="full name"
+          value={this.state.full_name}
+          onChange={this.handleFullNameChange}
+        />
+        <br />
+        {/* <input type="radio" value={this.state.organization_type}/><br/> */}
+        <input
+          type="text"
+          placeholder="website url"
+          value={this.state.website}
+          onChange={this.handleWebsiteChange}
+        />
+        <br />
+        <DatePicker
+          onChange={this.handleDateChange}
+          selected={this.state.running_since}
+        />
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        Add an organization
+        {this.showForm()}
+      </div>
+    );
+  }
 }
 
-export default OrgAddCard
+export default OrgAddCard;
