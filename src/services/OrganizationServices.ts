1db4dbec60a63f477368dbfb0cb43870ffb82b7d
oklex <alexkim.347@gmail.com>
2020-05-11 18:42:59 -0700

    added post api connection

--- src/services/OrganizationServices.ts
+++ src/services/OrganizationServices.ts
@@ -1,9 +1,24 @@
-import { IOrganization } from "../models/calendar";
+import { IOrganization, IOrganizationRequest } from "../models/calendar";
 import { calendarAPI } from "./constants";
 
 export const CalendarService = {
     async getAllOrganizations(): Promise<IOrganization[]> {
+        try {
         const { data } = await calendarAPI.get("/organizations/all")
         return data
+        } catch (err) {
+            console.log(err)
+            return []
+        }
+    },
+
+    async postNewOrganization(obj: IOrganizationRequest): Promise<string> {
+        try {
+            let response: string = await calendarAPI.post('/organizations/new', obj)
+            return response
+        } catch (err) {
+            console.log(err)
+            return err
+        }
     }
 }
