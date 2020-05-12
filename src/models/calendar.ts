868d537f0da79eb48b79d118d620f3d772b92f72
oklex <alexkim.347@gmail.com>
2020-05-12 16:49:28 -0700

    added appRequest type

--- src/models/calendar.ts
+++ src/models/calendar.ts
@@ -1,60 +1,69 @@
 
 // CC v2
 export interface ICalendarResponse {
-    organization: IOrganization,
-    events: IEvent[] | null,
-    applications: IApplication[] | null
-  }
-  
-  export interface IOrganization {
-    website_key: string;
-    short_name: string;
-    full_name: string;
-    organization_type: string;
-    website: string;
-    running_since: Date
-  }
+  organization: IOrganization,
+  events: IEvent[] | null,
+  applications: IApplication[] | null
+}
 
-  export interface IOrganizationRequest {
-    short_name?: string;
-    full_name?: string;
-    organization_type?: string;
-    website?: string;
-    running_since?: Date
-  }
-  
-  export enum IOrganizationType {
-    nonProfit = "Registered non profit",
-    schoolSponsored = "School Sponsored",
-    studentProject = "Student Project"
-  }
-  
-  export interface IEvent {
-    venue_name: string;
-    venue_city: string;
-    start_date: Date;
-    end_date: Date | null;
-    dates_tentative: boolean
-    tags: string[];
-  }
-  
-  export interface IApplication {
-    application_key: string
-    name: string
-    type: IApplicationType
-    start_date: Date
-    end_date: Date
-    dates_tentative: boolean
-    applicationLink: string
-    cost:number | null
-  }
-  
-  export enum IApplicationType {
-    Delegate = "Delegate Registration",
-    School = "School Registration",
-    Staff = "Staff Application",
-    Secretariat = "Secretariat Application",
-    Volunteer = "Volunteer Application",
-    Other = "Other"
-  }
-  
+export interface IOrganization {
+  website_key: string;
+  short_name: string;
+  full_name: string;
+  organization_type: string;
+  website: string;
+  running_since: Date
+}
+
+export interface IOrganizationRequest {
+  short_name?: string;
+  full_name?: string;
+  organization_type?: string;
+  website?: string;
+  running_since?: Date
+}
+
+export enum IOrganizationType {
+  nonProfit = "Registered non profit",
+  schoolSponsored = "School Sponsored",
+  studentProject = "Student Project"
+}
+
+export interface IEvent {
+  venue_name: string;
+  venue_city: string;
+  start_date: Date;
+  end_date: Date | null;
+  dates_tentative: boolean
+  tags: string[];
+}
+
+export interface IApplication {
+  application_key: string
+  name: string
+  type: IApplicationType
+  start_date: Date
+  end_date: Date
+  dates_tentative: boolean
+  applicationLink: string
+  cost: number | null
+}
+
+export interface IApplicationRequest {
+  name?: string;
+  type?: IApplicationType;
+  start_date?: Date;
+  end_date?: Date;
+  dates_tentative?: boolean;
+  applicationLink?: string;
+  cost?: number;
+}
+
+export enum IApplicationType {
+  Delegate = "Delegate Registration",
+  School = "School Registration",
+  Staff = "Staff Application",
+  Secretariat = "Secretariat Application",
+  Volunteer = "Volunteer Application",
+  Other = "Other"
+}
