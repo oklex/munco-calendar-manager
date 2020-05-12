c4895e5ca0dfdf7b2896d7f94c2185230a49772a
oklex <alexkim.347@gmail.com>
2020-05-12 15:29:01 -0700

    got route params working

--- src/containers/EditorPanel/EditorPanel.tsx
+++ src/containers/EditorPanel/EditorPanel.tsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import AppEditCard from '../../components/applicationCards/appEditCard'
-import OrgEditCard from '../../components/OrganizationCards/orgEditCard'
+import React from "react";
+import AppEditCard from "../../components/applicationCards/appEditCard";
+import OrgEditCard from "../../components/OrganizationCards/orgEditCard";
+import "./EditorPanel.scss";
+import { IOrganization } from "../../models/calendar";
+import { RouteComponentProps, withRouter } from "react-router-dom";
 
-export default class EditorPanel extends React.Component<{},{}> {
-    render() {
-        return (
-            <div>EditorPanel prototype
-                <OrgEditCard/>
-                <AppEditCard/>
-            </div>
-        )
-    }
+interface MatchParams {
+  website_key: string
 }
+
+class EditorPanel extends React.Component<RouteComponentProps<MatchParams>> {
+
+  componentDidMount = () => {
+    let key: string =this.props.match.params.website_key
+    console.log(key)
+    this.setState({
+        website_key: key
+    })
+  };
+
+  render() {
+    return (
+      <div className="editor container">
+        <p className="miniText">Editor view for ...</p>
+        <OrgEditCard />
+        <AppEditCard />
+      </div>
+    );
+  }
+}
+
+export default withRouter(EditorPanel)
