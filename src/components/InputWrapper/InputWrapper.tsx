b0d5536c8f62aded097bf5544e61bf220a043add
oklex <alexkim.347@gmail.com>
2020-05-12 11:39:40 -0700

    added accompanying scss file

--- src/components/InputWrapper/InputWrapper.tsx
+++ src/components/InputWrapper/InputWrapper.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import './InputWrapper.scss'
 
 interface IInputWrapperProps {
   title: string;
+  subtitle?: string;
   warning: string | null;
   children: JSX.Element;
 }
@@ -9,8 +11,9 @@ interface IInputWrapperProps {
 export class InputWrapper extends React.Component<IInputWrapperProps> {
   render() {
     return (
-      <div>
-          <p><strong>{this.props.title}</strong></p>
+      <div className='inputWrapper'>
+          <p>{this.props.title}</p>
+          <p className='miniText'>{this.props.subtitle? this.props.subtitle : ""}</p>
         {this.props.children}
         <p className='miniText errorText'>{this.props.warning ? this.props.warning : ""}</p>
       </div>
