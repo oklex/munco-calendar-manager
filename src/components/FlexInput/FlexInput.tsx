21c1ddfffba8e155038e56b8899cb97dd94086c0
oklex <alexkim.347@gmail.com>
2020-05-13 18:17:58 -0700

    added placeholder text

--- src/components/FlexInput/FlexInput.tsx
+++ src/components/FlexInput/FlexInput.tsx
@@ -4,136 +4,140 @@ import { IApplicationType } from "../../models/calendar";
 import { matchAppType } from "../../utils/MatchAppType";
 
 interface IFlexInputProps {
-	placeholder: string;
-	onChange: (val: string) => string;
-	refresh?: number;
-	type?: IAcceptedInputTypes;
+  placeholder: string;
+  onChange: (val: string) => string;
+  refresh?: number;
+  type?: IAcceptedInputTypes;
 }
 
 export enum IAcceptedInputTypes {
-	text = "text",
-	applicationTypes = "applicationTypes",
-	checkbox = "checkbox",
+  text = "text",
+  applicationTypes = "applicationTypes",
+  checkbox = "checkbox",
 }
 
 interface IFlexInputState {
-	value: string;
-	errorMessage: string;
-	showInput: boolean;
+  value: string;
+  errorMessage: string;
+  showInput: boolean;
 }
 
 class FlexInput extends React.Component<IFlexInputProps, IFlexInputState> {
-	state = {
-		value: "",
-		errorMessage: "",
-		showInput: false,
-	};
+  state = {
+    value: "",
+    errorMessage: "",
+    showInput: false,
+  };
 
-	componentDidMount = () => {
-		this.setState({
-			value: this.props.placeholder,
-		});
-	};
+  componentDidMount = () => {
+    this.setState({
+      value: this.props.placeholder,
+    });
+  };
 
-	componentDidUpdate = (
-		prevProps: IFlexInputProps,
-		prevState: IFlexInputState
-	) => {
-		if (prevProps.refresh !== this.props.refresh) {
-			this.setState({
-				value: this.props.placeholder,
-				errorMessage: "",
-				showInput: false,
-			});
-		}
-	};
+  componentDidUpdate = (
+    prevProps: IFlexInputProps,
+    prevState: IFlexInputState
+  ) => {
+    if (prevProps.refresh !== this.props.refresh) {
+      this.setState({
+        value: this.props.placeholder,
+        errorMessage: "",
+        showInput: false,
+      });
+    }
+  };
 
-	toggleInputOn = (e: any) => {
-		this.setState({
-			showInput: true,
-		});
-	};
+  toggleInputOn = (e: any) => {
+    this.setState({
+      showInput: true,
+    });
+  };
 
-	toggleInputOff = (e?: any) => {
-		this.setState({
-			showInput: false,
-		});
-	};
+  toggleInputOff = (e?: any) => {
+    this.setState({
+      showInput: false,
+    });
+  };
 
-	onInputChange = (e: React.FormEvent<HTMLInputElement>) => {
-		this.setState({
-			value: e.currentTarget.value,
-			errorMessage: this.props.onChange(e.currentTarget.value),
-		});
-	};
+  onInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    this.setState({
+      value: e.currentTarget.value,
+      errorMessage: this.props.onChange(e.currentTarget.value),
+    });
+  };
 
-	onSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-		console.log(e.target.value);
-			let value: string = e.target.value;
-			this.setState({
-				value: e.currentTarget.value,
-				errorMessage: this.props.onChange(value),
-			});
-	};
+  onSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(e.target.value);
+    let value: string = e.target.value;
+    this.setState({
+      value: e.currentTarget.value,
+      errorMessage: this.props.onChange(value),
+    });
+  };
 
-	onKeyDown = (e: any) => {
-		if (e.key === "Enter") {
-			this.toggleInputOff();
-		}
-	};
+  onKeyDown = (e: any) => {
+    if (e.key === "Enter") {
+      this.toggleInputOff();
+    }
+  };
 
-	showInput = () => {
-		if (!this.props.type || this.props.type === "text") {
-			return (
-				<div onBlur={this.toggleInputOff}>
-					<input
-						autoFocus
-						type="text"
-						value={this.state.value}
-						onChange={this.onInputChange}
-						onKeyDown={this.onKeyDown}
-						onBlur={this.toggleInputOff}
-					/>
-				</div>
-			);
-		} else if (this.props.type === "applicationTypes") {
-			return (
-				<div onBlur={this.toggleInputOff}>
-					<select
-						autoFocus
-						value={this.state.value}
-						onChange={this.onSelectChange}
-						onKeyDown={this.onKeyDown}
-						onBlur={this.toggleInputOff}
-					>
-						<option value={IApplicationType.Staff}>Staff</option>
-						<option value={IApplicationType.Delegate}>Delegate</option>
-						<option value={IApplicationType.Secretariat}>Secretariat</option>
-						<option value={IApplicationType.School}>School</option>
-						<option value={IApplicationType.Volunteer}>Volunteer</option>
-						<option value={IApplicationType.Other}>Other</option>
-					</select>
-				</div>
-			);
-		}
-	};
+  showInput = () => {
+    if (!this.props.type || this.props.type === "text") {
+      return (
+        <div onBlur={this.toggleInputOff}>
+          <input
+            autoFocus
+            type="text"
+            value={this.state.value}
+            onChange={this.onInputChange}
+            onKeyDown={this.onKeyDown}
+            onBlur={this.toggleInputOff}
+          />
+        </div>
+      );
+    } else if (this.props.type === "applicationTypes") {
+      return (
+        <div onBlur={this.toggleInputOff}>
+          <select
+            autoFocus
+            value={this.state.value}
+            onChange={this.onSelectChange}
+            onKeyDown={this.onKeyDown}
+            onBlur={this.toggleInputOff}
+          >
+            <option value={IApplicationType.Staff}>Staff</option>
+            <option value={IApplicationType.Delegate}>Delegate</option>
+            <option value={IApplicationType.Secretariat}>Secretariat</option>
+            <option value={IApplicationType.School}>School</option>
+            <option value={IApplicationType.Volunteer}>Volunteer</option>
+            <option value={IApplicationType.Other}>Other</option>
+          </select>
+        </div>
+      );
+    }
+  };
 
-	showDisplay = () => {
-		if (this.state.showInput) {
-			return this.showInput();
-		} else {
-			return <div className="flexInput" onClick={this.toggleInputOn}>{this.state.value}</div>;
-		}
-	};
+  showDisplay = () => {
+    if (this.state.showInput) {
+      return this.showInput();
+    } else {
+      return (
+        <div className="flexInput" onClick={this.toggleInputOn}>
+          {this.state.value ? this.state.value : <p className='neutralText'>input here</p>}
+        </div>
+      );
+    }
+  };
 
-	render() {
-		return (
-			<div>
-				{this.showDisplay()}
-				<p className="miniText errorText">{this.state.errorMessage}</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        {this.showDisplay()}
+        <p className="miniText errorText">{this.state.errorMessage}</p>
+      </div>
+    );
+  }
 }
 
 export default FlexInput;
